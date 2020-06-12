b480e444d85120a61fb34bc11316d5ef2cbb3755
suvrick <suvrick@gmail.com>
2020-06-12 17:01:57 +0700

    update app

---
+++ build/precache-manifest.914028845432cee5af4a0e129b172016.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a5f1aa7770f21809e1e3d60cd1d426e6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "75bc5bd4f28de9afaa99",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "279bc56b892467eecae1",
+    "url": "/static/js/2.280678da.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.280678da.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "75bc5bd4f28de9afaa99",
+    "url": "/static/js/main.ed3561d8.chunk.js"
+  },
+  {
+    "revision": "73bbfc2cab02074f6794",
+    "url": "/static/js/runtime-main.a5916c29.js"
+  }
+]);
---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+
+
+function App() {
+  return (
+    <div className="App">
+      Hello 2
+    </div>
+  );
+}
+
+export default App;
